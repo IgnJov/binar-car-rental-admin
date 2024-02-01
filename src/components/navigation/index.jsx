a1df23c9600a48ddd58c5dc5d9b4acd86eac33a9
Ignatius Jovanka <jovankaignatius@gmail.com>
2024-02-01 20:05:27 +0700

    Add Static user dropdown with logout function

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -14,60 +14,107 @@ import searchIcon from "../../assets/search-icon.svg";
 // Libraries
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import InputGroupText from "react-bootstrap/esm/InputGroupText";
 
 function Navigation() {
-  const [search, setSearch] = useState("");
+    const [search, setSearch] = useState("");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleSidebarToggle = () => {
-    document.querySelector("#sidebar .sub-menu-container").classList.toggle("d-none");
-  };
+    const handleSidebarToggle = () => {
+        document
+            .querySelector("#sidebar .sub-menu-container")
+            .classList.toggle("d-none");
+    };
 
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+    };
 
-  const handleSearchSubmit = () => {
-    const queryString = search.trim() != "" ? `?name=${search}` : "";
-    navigate(`/cars/list-car` + queryString);
-    location.reload();
-  };
+    const handleSearchSubmit = () => {
+        const queryString = search.trim() != "" ? `?name=${search}` : "";
+        navigate(`/cars/list-car` + queryString);
+        location.reload();
+    };
 
-  return (
-    <Navbar className="shadow-sm">
-      <Container fluid>
-        <Navbar.Brand>
-          <Button variant="outline-light" onClick={handleSidebarToggle}>
-            <img src={hamburgerMenu} />
-          </Button>
-        </Navbar.Brand>
-        <Form className="d-flex ms-auto">
-          <InputGroup>
-            <InputGroup.Text>
-              <img src={searchIcon} alt="search icon" />
-            </InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="search"
-              onChange={handleSearchChange}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  e.preventDefault();
-                  handleSearchSubmit(e.target.value);
-                }
-              }}
-            />
-            <Button variant="outline-primary" onClick={handleSearchSubmit}>
-              Search
-            </Button>
-          </InputGroup>
-        </Form>
-      </Container>
-    </Navbar>
-  );
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+    };
+
+    return (
+        <Navbar className="shadow-sm" id="navigation-bar">
+            <Container fluid>
+                <Navbar.Brand>
+                    <Button
+                        variant="outline-light"
+                        onClick={handleSidebarToggle}
+                    >
+                        <img src={hamburgerMenu} />
+                    </Button>
+                </Navbar.Brand>
+                <Form className="d-flex align-items-center">
+                    <InputGroup
+                        className="d-flex flex-row me-4"
+                        style={{ minWidth: "300px" }}
+                    >
+                        <InputGroup.Text>
+                            <img src={searchIcon} alt="search icon" />
+                        </InputGroup.Text>
+                        <Form.Control
+                            style={{ width: "100px" }}
+                            type="text"
+                            placeholder="Search"
+                            aria-label="Search"
+                            aria-describedby="search"
+                            onChange={handleSearchChange}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                    e.preventDefault();
+                                    handleSearchSubmit(e.target.value);
+                                }
+                            }}
+                        />
+                        <Button
+                            variant="outline-primary"
+                            onClick={handleSearchSubmit}
+                        >
+                            Search
+                        </Button>
+                    </InputGroup>
+                    <InputGroup>
+                        <InputGroupText
+                            style={{
+                                backgroundColor: "transparent",
+                                border: "0",
+                            }}
+                            className="m-0 p-0 me-2"
+                        >
+                            <svg
+                                width="30"
+                                height="30"
+                                viewBox="0 0 38 38"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <circle cx="19" cy="19" r="19" fill="#CFD4ED" />
+                            </svg>
+                        </InputGroupText>
+                        <DropdownButton
+                            id="dropdown-user"
+                            variant="outline-dark"
+                            title="Admin"
+                            align="end"
+                        >
+                            <Dropdown.Item href="#" onClick={handleLogout}>
+                                Logout
+                            </Dropdown.Item>
+                        </DropdownButton>
+                    </InputGroup>
+                </Form>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Navigation;
